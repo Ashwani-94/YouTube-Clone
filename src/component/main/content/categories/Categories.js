947bcb2f7b9c93ff337b6dde3fd6fc14c04c2297
Ashwani Kumar <ashwamikumar94665@gmail.com>
2021-06-10 17:18:48 +0530

    minor bugs fixed

--- src/component/main/content/categories/Categories.js
+++ src/component/main/content/categories/Categories.js
@@ -5,20 +5,20 @@ export const Categories = () => {
         <div className={Categories_css.categories}>
             <div id={Categories_css.categories_item_active} className={Categories_css.categories_item}>All</div>
             <div className={Categories_css.categories_item}>Science</div>
-            <div className={Categories_css.categories_item}>Ashwani_Kumar</div>
-            <div className={Categories_css.categories_item}>Roorkee</div>
-            <div className={Categories_css.categories_item}>React</div>
+            <div className={Categories_css.categories_item}>Ashwani Kumar</div>
+            <div className={Categories_css.categories_item}>IIT Roorkee</div>
+            <div className={Categories_css.categories_item}>React JS</div>
             <div className={Categories_css.categories_item}>Youtube</div>
             <div className={Categories_css.categories_item}>Live</div>
             <div className={Categories_css.categories_item}>Java_Script</div>
             <div className={Categories_css.categories_item}>HTML</div>
             <div className={Categories_css.categories_item}>CSS</div>
             <div className={Categories_css.categories_item}>Angular</div>
-            <div className={Categories_css.categories_item}>ASME</div>
-            <div className={Categories_css.categories_item}>Devlopment</div>
+            <div className={Categories_css.categories_item}>ASME IITR Chapter</div>
+            <div className={Categories_css.categories_item}>Web Devlopment</div>
             <div className={Categories_css.categories_item}>Software</div>
             <div className={Categories_css.categories_item}>Coding</div>
-            <div className={Categories_css.categories_item}>ICPC</div>
+            <div className={Categories_css.categories_item}>ACM ICPC</div>
             <div className={Categories_css.categories_item}>C++</div>
             <div className={Categories_css.categories_item}>Python</div>
         </div>
